7cd866245aa3933d54bcbad9982a3581036b07bb
myingadam-creator <myingadam@gmail.com>
2026-05-26 10:48:07 +0300

    Create main.py for Elimu Yetu App

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class ElimuYetApp(App):
+    def build(self):
+        return Label(text='Elimu Yetu App Inafanya Kazi Mkuu!')
+
+ElimuYetApp().run()
